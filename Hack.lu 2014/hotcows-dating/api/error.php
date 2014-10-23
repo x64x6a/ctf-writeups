8d542ce32f9831ecb07208f0f599ef3700ff2114
x64x6a <darekjens@gmail.com>
2014-10-23 11:07:59 -0500

    a
    a

--- Hack.lu 2014/hotcows-dating/api/error.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('MAIN')) die();
-
-
-$t = new Template(TEMPLATE_DIR . 'message.pjson');
-$t->assign('404 - Not Found.', 'message');
-$t->assign(false, 'success');
-$t->render();
--- Hack.lu 2014/hotcows-dating/lib/util.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('MAIN')) die();
-
-
-function http_redirect($location) {
-    header('Location: ' . $location);
-}
